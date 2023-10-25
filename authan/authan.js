0d262a22c1ea367c40b0878eb8dd0289885c6c9e
Silence-Mac <princedibloni4@gmail.com>
2023-10-25 16:20:03 +0000

    done

--- authan/authan.js
+++ authan/authan.js
@@ -1,14 +1,3 @@
-function onSignIn(googleUser) {
-    console.log("called");
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-
-
-
 const authantificationRequest = async (method, endpoint, data = null) => {
     const options = {
         method,
