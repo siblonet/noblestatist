0ff9f348a625a1b11fa7d452cea9957a9c919d15
Silence-Mac <princedibloni4@gmail.com>
2023-11-03 01:17:58 +0000

    done

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const apiUrlfin = 'http://localhost:3000/';
 const apiUrlfine = 'https://zany-plum-bear.cyclic.cloud/';
-const tunal = "https://zany-plum-bear.cyclic.cloud/"
-const tunalS = "http://localhost:3000/"
+const tunalS = "https://liveshopping.adaptable.app/"
+const tunals = "http://localhost:3000/"
 
 const token = 'YOUR_TOKEN_HERE'; // Replace with your actual token
