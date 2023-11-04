c2deac3d4001ef49d6269d3d0f78499b38feda6b
Silence-Mac <princedibloni4@gmail.com>
2023-11-04 17:46:43 +0000

    done

--- livechat.js
+++ livechat.js
@@ -1,6 +1,3 @@
-let sockectState = false;
-const socket = io(tunalS);
-
 function generateUUID() {
     return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         const r = (Math.random() * 16) | 0;
@@ -11,53 +8,3 @@ function generateUUID() {
 
 let myLive = generateUUID();
 
-socket.connect();
-
-socket.on('connect', () => {
-        sockectState = true;
-
-});
-
-
-socket.on('disconnect', () => {
-    sockectState = false;
-});
-
-
-socket.on('reconnect_attempt', () => {
-    sockectState = true;
-
-  console.log('Tentative de reconnexion...');
-});
-
-socket.on('reconnect', (attemptNumber) => {
-    sockectState = true;
-
-  console.log(`Rétabli la connexion après ${attemptNumber} tentatives.`);
-});
-
-
-
-
-const socketWithOptions = io(tunalS, {
-  reconnection: true, // Activer la reconnexion automatique
-  reconnectionAttempts: 3, // Nombre maximal de tentatives de reconnexion
-  reconnectionDelay: 1000, // Délai initial avant la première tentative de reconnexion (en millisecondes)
-  reconnectionDelayMax: 5000, // Délai maximal entre les tentatives de reconnexion (en millisecondes)
-  randomizationFactor: 0.5, // Facteur de randomisation pour les délais de reconnexion
-});
-
-socketWithOptions.connect();
-
-socket.on('reconnect_error', (error) => {
-    sockectState = false;
-
-  console.error('Erreur de reconnexion:', error);
-  setTimeout(() => {
-    socket.connect();
-    socket.on('connect', () => {
-        sockectState = true;
-
-});
-  }, 99990);
-});
