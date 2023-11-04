c40d945f80f1a3f379831434b0309c2c64d17f7a
Silence-Mac <princedibloni4@gmail.com>
2023-11-04 17:58:18 +0000

    done

--- livechat.js
+++ livechat.js
@@ -1,17 +1,9 @@
-let sockectState = false;
-let myLive = "";
-const socket = io(tunalS);
-/*function generateUUID() {
-    return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        const r = (Math.random() * 16) | 0;
-        const v = c === 'x' ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-    });
+function generateUUID() {
+  return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
 let myLive = generateUUID();
-
-
-socket.on('disconnect', () => {
-    sockectState = false;
-});*/
